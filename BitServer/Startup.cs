e55f59ac9c1c291d590bfaa529e25b8ded33bd90
User <itaykoski1@gmail.com>
2021-10-25 13:05:26 +0300

    2

--- BitServer/Startup.cs
+++ BitServer/Startup.cs
@@ -7,34 +7,65 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using BitServerBL.Models;
 
 namespace BitServer
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
+            // This method gets called by the runtime. Use this method to add services to the container.
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+            public Startup(IConfiguration configuration)
             {
-                app.UseDeveloperExceptionPage();
+                Configuration = configuration;
+            }
+            public IConfiguration Configuration { get; }
+            public void ConfigureServices(IServiceCollection services)
+            {
+                //Add Controllers and set the Json Serializer to handle loop referencing
+                services.AddControllers().AddJsonOptions(o => o.JsonSerializerOptions
+                            .ReferenceHandler = ReferenceHandler.Preserve);
+                //The following two commands set the Session state to work!
+                services.AddDistributedMemoryCache();
+
+                services.AddSession(options =>
+                {
+                    options.IdleTimeout = TimeSpan.FromMinutes(180);
+                    options.Cookie.HttpOnly = true;
+                    options.Cookie.IsEssential = true;
+                });
+
+                //The following set the connection string to the DB and DB context!
+                #region Add DB Context Support
+                string connectionString = this.Configuration.GetConnectionString("YadBeYadDB");
+
+                services.AddDbContext<BitDBContext>(options => options
+                                                                    .UseSqlServer(connectionString));
+                //.UseLazyLoadingProxies());
+                #endregion
             }
 
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
+            // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+            public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
             {
-                endpoints.MapGet("/", async context =>
+                if (env.IsDevelopment())
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    app.UseDeveloperExceptionPage();
+                }
+
+                app.UseStaticFiles(); //Added to have the wwwroot folder and server to accept calls to static files
+                app.UseRouting();
+                app.UseSession(); //Added to tell the server to use sessions!
+
+                app.UseEndpoints(endpoints =>
+                {
+                    endpoints.MapControllers();
                 });
-            });
-        }
+            }
     }
 }

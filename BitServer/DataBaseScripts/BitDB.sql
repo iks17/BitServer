f2feace45eaad4641c8d45e5e4ecca54e09a673e
User <User@LAB2-16>
2021-11-07 07:57:38 +0200

    1

--- BitServer/DataBaseScripts/BitDB.sql
+++ BitServer/DataBaseScripts/BitDB.sql
@@ -1,99 +1,98 @@
-﻿Use Master
+﻿Use master
 Go
 Create Database BitDB
 GO
-CREATE TABLE Users(
-    Email NVARCHAR(255) NOT NULL,
-    UserPW NVARCHAR(255) NOT NULL,
-    UserID INT NOT NULL,
-    PhoneNumber INT NOT NULL,
-    RegistartionDate DATE NOT NULL
+
+CREATE TABLE "Users"(
+    "Email" NVARCHAR(255) NOT NULL,
+    "Password" NVARCHAR(255) NOT NULL,
+    "UserID" INT NOT NULL,
+    "Phone Number" INT NOT NULL,
+    "RegistartionDate" DATE NOT NULL
 );
 ALTER TABLE
-    Users ADD CONSTRAINT user_userid_primary PRIMARY KEY(UserID);
-CREATE TABLE Customer(
-    UserID INT NOT NULL,
-    UAddress NVARCHAR(255) NOT NULL,
-    Country NVARCHAR(255) NOT NULL,
-    FirstName NVARCHAR(255) NOT NULL,
-    Gender NCHAR(255) NOT NULL,
-    CustomerType INT NOT NULL
+    "Users" ADD CONSTRAINT "users_userid_primary" PRIMARY KEY("UserID");
+CREATE TABLE "Customers"(
+    "CustomerID" INT NOT NULL,
+    "Address" NVARCHAR(255) NOT NULL,
+    "Country" NVARCHAR(255) NOT NULL,
+    "First Name" NVARCHAR(255) NOT NULL,
+    "Gender" NCHAR(255) NOT NULL,
+    "UserID" INT NOT NULL
 );
 ALTER TABLE
-    Customers ADD CONSTRAINT customer_userid_primary PRIMARY KEY(UserID);
-CREATE TABLE Admin(
-    UserID INT NOT NULL,
-    IsActiveAdmin INT NOT NULL,
-    EnrolledCustomers INT NOT NULL,
-    LastOnline DATE NOT NULL
+    "Customers" ADD CONSTRAINT "customers_customerid_primary" PRIMARY KEY("CustomerID");
+CREATE TABLE "Admins"(
+    "AdminID" INT NOT NULL,
+    "EnrolledCustomers" INT NOT NULL,
+    "LastOnline" DATE NOT NULL,
+    "UserID" INT NOT NULL
 );
 ALTER TABLE
-    UAdmin ADD CONSTRAINT admin_userid_primary PRIMARY KEY(UserID);
-ALTER TABLE
-    UAdmin ADD CONSTRAINT admin_isactiveadmin_primary PRIMARY KEY(IsActiveAdmin);
-CREATE TABLE BusinessAccount(
-    MainUserID INT NOT NULL,
-    BusinessEmail NVARCHAR(255) NOT NULL,
-    BusinessPassword NVARCHAR(255) NOT NULL,
-    BusinessName NVARCHAR(255) NOT NULL,
-    ActiveManagersNum INT NOT NULL,
-    BusibessAdress NVARCHAR(255) NOT NULL,
-    AnualIncome INT NOT NULL,
-    NetWorth INT NOT NULL,
-    BusinessOpenDate DATE NOT NULL,
-    MainCurrency NCHAR(255) NOT NULL,
-    TotalBalance FLOAT NOT NULL
+    "Admins" ADD CONSTRAINT "admins_adminid_primary" PRIMARY KEY("AdminID");
+CREATE TABLE "BusinessAccounts"(
+    "AccountID" NVARCHAR(255) NOT NULL,
+    "BusinessEmail" NVARCHAR(255) NOT NULL,
+    "BusinessPassword" NVARCHAR(255) NOT NULL,
+    "BusinessName" NVARCHAR(255) NOT NULL,
+    "ActiveManagersNum" INT NOT NULL,
+    "BusibessAdress" NVARCHAR(255) NOT NULL,
+    "Anual Income" INT NOT NULL,
+    "NetWorth" INT NOT NULL,
+    "BusinessOpenDate" DATE NOT NULL,
+    "MainCurrency" NCHAR(255) NOT NULL,
+    "TotalBalance" FLOAT NOT NULL,
+    "CustomerID" INT NOT NULL
 );
 ALTER TABLE
-    BusinessAccount ADD CONSTRAINT businessaccount_mainuserid_primary PRIMARY KEY(MainUserID);
-CREATE TABLE PrivateAccount(
-    UserID INT NOT NULL,
-    DateOfBirth DATE NOT NULL,
-    MainCurrency NCHAR(255) NOT NULL,
-    AnualIncome INT NOT NULL,
-    TotalBalance FLOAT NOT NULL
+    "BusinessAccounts" ADD CONSTRAINT "businessaccounts_accountid_primary" PRIMARY KEY("AccountID");
+CREATE TABLE "PrivateAccounts"(
+    "AccountID" NVARCHAR(255) NOT NULL,
+    "DateOfBirth" DATE NOT NULL,
+    "MainCurrency" NCHAR(255) NOT NULL,
+    "AnualIncome" INT NOT NULL,
+    "TotalBalance" FLOAT NOT NULL,
+    "CustomerID" INT NOT NULL
 );
 ALTER TABLE
-    PrivateAccount ADD CONSTRAINT privateaccount_userid_primary PRIMARY KEY(UserID);
-CREATE TABLE Loan(
-    UserID INT NOT NULL,
-    LoanID INT NOT NULL,
-    UserBalance FLOAT NOT NULL,
-    Balance FLOAT NOT NULL
+    "PrivateAccounts" ADD CONSTRAINT "privateaccounts_accountid_primary" PRIMARY KEY("AccountID");
+CREATE TABLE "Loans"(
+    "LoanID" INT NOT NULL,
+    "UserBalance" FLOAT NOT NULL,
+    "Balance" FLOAT NOT NULL,
+    "ApprovedBy" INT NOT NULL,
+    "BusinessID" NVARCHAR(255) NOT NULL,
+    "PrivateID" NVARCHAR(255) NOT NULL
 );
 ALTER TABLE
-    Loan ADD CONSTRAINT loan_userid_primary PRIMARY KEY(UserID);
-CREATE TABLE TransactionLogs(
-    TransactionID INT NOT NULL,
-    SentAmt FLOAT NOT NULL,
-    SenderID INT NOT NULL,
-    ReceiverID INT NOT NULL,
-    SenderType INT NOT NULL,
-    ReceiverType INT NOT NULL
+    "Loans" ADD CONSTRAINT "loans_loanid_primary" PRIMARY KEY("LoanID");
+CREATE TABLE "TransactionLogs"(
+    "TransactionID" INT NOT NULL,
+    "SentAmt" INT NOT NULL,
+    "SenderID" INT NOT NULL,
+    "ReceiverID" INT NOT NULL,
+    "SenderAccount" NVARCHAR(255) NOT NULL,
+    "ReceiverAccount" NVARCHAR(255) NOT NULL,
+    "TransactionDate" DATE NOT NULL,
+    "IsConfirmed" BIT NOT NULL
 );
 ALTER TABLE
-    TransactionLogs ADD CONSTRAINT transactionlogs_transactionid_primary PRIMARY KEY(TransactionID);
-CREATE TABLE CostumerType(
-    id INT NOT NULL,
-    UDesc NVARCHAR(255) NOT NULL
-);
+    "TransactionLogs" ADD CONSTRAINT "transactionlogs_transactionid_primary" PRIMARY KEY("TransactionID");
 ALTER TABLE
-    CostumerType ADD CONSTRAINT costumertype_id_primary PRIMARY KEY(id);
+    "Admins" ADD CONSTRAINT "admins_userid_foreign" FOREIGN KEY("UserID") REFERENCES "Users"("UserID");
 ALTER TABLE
-    TransactionLogs ADD CONSTRAINT transactionlogs_sentamt_foreign FOREIGN KEY(SentAmt) REFERENCES Customer(UserID);
+    "Customers" ADD CONSTRAINT "customers_userid_foreign" FOREIGN KEY("UserID") REFERENCES "Users"("UserID");
 ALTER TABLE
-    TransactionLogs ADD CONSTRAINT transactionlogs_senderid_foreign FOREIGN KEY(SenderID) REFERENCES Customer(UserID);
+    "BusinessAccounts" ADD CONSTRAINT "businessaccounts_customerid_foreign" FOREIGN KEY("CustomerID") REFERENCES "Customers"("CustomerID");
 ALTER TABLE
-    BusinessAccount ADD CONSTRAINT businessaccount_businessemail_foreign FOREIGN KEY(BusinessEmail) REFERENCES Customer(UserID);
+    "TransactionLogs" ADD CONSTRAINT "transactionlogs_receiverid_foreign" FOREIGN KEY("ReceiverID") REFERENCES "Customers"("CustomerID");
 ALTER TABLE
-    Customer ADD CONSTRAINT customer_customertype_foreign FOREIGN KEY(CustomerType) REFERENCES CostumerType(id);
-
-
-
-
-
-
-
-
-
-
+    "PrivateAccounts" ADD CONSTRAINT "privateaccounts_customerid_foreign" FOREIGN KEY("CustomerID") REFERENCES "Customers"("CustomerID");
+ALTER TABLE
+    "TransactionLogs" ADD CONSTRAINT "transactionlogs_senderid_foreign" FOREIGN KEY("SenderID") REFERENCES "Customers"("CustomerID");
+ALTER TABLE
+    "Loans" ADD CONSTRAINT "loans_approvedby_foreign" FOREIGN KEY("ApprovedBy") REFERENCES "Admins"("AdminID");
+ALTER TABLE
+    "Loans" ADD CONSTRAINT "loans_businessid_foreign" FOREIGN KEY("BusinessID") REFERENCES "BusinessAccounts"("AccountID");
+ALTER TABLE
+    "Loans" ADD CONSTRAINT "loans_privateid_foreign" FOREIGN KEY("PrivateID") REFERENCES "PrivateAccounts"("AccountID");

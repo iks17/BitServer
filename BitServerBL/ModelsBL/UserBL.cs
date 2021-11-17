37a995d8cdec425eff72475b3fadae7747586fdc
222User <User@LAB2-16>
2021-11-17 11:02:15 +0200

    444

--- BitServerBL/ModelsBL/UserBL.cs
+++ BitServerBL/ModelsBL/UserBL.cs
@@ -3,20 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BitServerBL.Models;
+using Microsoft.EntityFrameworkCore;
 
-namespace BitServerBL.ModelsBL
+namespace BitServerBL.Models
 {
-    class UserBL:BitDBContextBL
+    class UserBL:BitDBContext
     {
         public User Login(string email, string pswd)
         {
-            Users user = this.Users
-                .Include(us => us.)
-                .Include(uc => uc.)
+            User user = this.Users
+                .Include(us => us.Admins)
+                .Include(uc => uc.Customers)
                 .Where(u => u.Email == email && u.Pass == pswd).FirstOrDefault();
 
             return user;
         }
     }
 }
-}
+

f7382c2035eabdf95a679ff4acc76fac0bbe47e5
User <User@LAB2-16>
2022-02-28 14:06:17 +0200

    2321

--- BitServer/Controllers/BitController.cs
+++ BitServer/Controllers/BitController.cs
@@ -20,14 +20,7 @@ namespace BitServer.Controllers
             this.context = context;
         }
         #endregion
-        [Route("Test")]
-        [HttpGet]
-        [HttpGet]
-        public string Test()
-        {
-            Response.StatusCode = (int)System.Net.HttpStatusCode.OK;
-            return context.Test();
-        }
+      
         [Route("Login")]
         [HttpGet]
         public User Login([FromQuery] string email, [FromQuery] string pass)
